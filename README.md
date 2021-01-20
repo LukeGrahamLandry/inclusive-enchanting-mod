86e1f521441d92b97ad1ff59aa56e343eb832bd0
Luke Graham Landry <graham11000@gmail.com>
2021-01-20 13:12:53 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Inclusive Enchanting Mod 
+
+A Minecraft mod for forge 1.16.5 that allows some enchantments to effect extra items. 
+
+## Features
+
---
+++ src/main/java/io/github/lukegrahamlandry/inclusiveenchanting/InclusiveEnchanting.java
@@ -0,0 +1,19 @@
+package io.github.lukegrahamlandry.inclusiveenchanting;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("inclusiveenchanting")
+public class InclusiveEnchanting{
+    // Directly reference a log4j logger.
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "inclusiveenchanting";
+
+    public InclusiveEnchanting() {
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
