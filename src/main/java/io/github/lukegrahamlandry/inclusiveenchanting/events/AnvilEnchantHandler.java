d53595a083b27ecbadf6d8465675d1609b14fe8c
Luke Graham Landry <graham11000@gmail.com>
2021-01-25 16:08:00 -0500

    Fix Repairing and Renaming

--- src/main/java/io/github/lukegrahamlandry/inclusiveenchanting/events/AnvilEnchantHandler.java
+++ src/main/java/io/github/lukegrahamlandry/inclusiveenchanting/events/AnvilEnchantHandler.java
@@ -5,12 +5,13 @@ import io.github.lukegrahamlandry.inclusiveenchanting.InclusiveEnchanting;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
-import net.minecraft.item.BowItem;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ToolItem;
+import net.minecraft.inventory.container.RepairContainer;
+import net.minecraft.item.*;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.event.AnvilUpdateEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -38,7 +39,17 @@ public class AnvilEnchantHandler {
     }
 
     @SubscribeEvent
-    public static void handleNewEnchants(AnvilUpdateEvent event){
+    public static void handleAnvil(AnvilUpdateEvent event){
+        if (isWrongItem(event)) return;
+        doNewEnchants(event);
+        doRepairRename(event);
+    }
+
+    private static boolean isWrongItem(AnvilUpdateEvent event) {
+        return !event.getRight().isEmpty() && event.getRight().isDamageable() && event.getLeft().getItem() != event.getRight().getItem();
+    }
+
+    public static void doNewEnchants(AnvilUpdateEvent event){
         ItemStack tool = event.getLeft();
         ItemStack add = event.getRight();
         Map<Enchantment, Integer> addEnchants = EnchantmentHelper.getEnchantments(add);
@@ -47,6 +58,8 @@ public class AnvilEnchantHandler {
         Map<Enchantment, Integer> newEnchantments = new HashMap<>();
 
         AtomicInteger totalCost = new AtomicInteger(0);
+
+        boolean isBook = add.getItem() == Items.ENCHANTED_BOOK && !EnchantedBookItem.getEnchantments(add).isEmpty();
 
         if (tool.getItem().isEnchantable(tool) && !addEnchants.isEmpty()){
             addEnchants.forEach((enchant, level) -> {
@@ -84,19 +97,19 @@ public class AnvilEnchantHandler {
                         if (level.equals(toolEnchants.get(enchant))){
                             if (level + 1 <= enchant.getMaxLevel()){
                                 newEnchantments.put(enchant, level + 1);
-                                totalCost.addAndGet(getXPCost(enchant));
+                                totalCost.addAndGet(getXPCost(enchant, level, isBook));
                             }
 
-                        // if the new one is larger than the old
+                            // if the new one is larger than the old
                         } else if (level > toolEnchants.get(enchant)){
                             newEnchantments.put(enchant, level);
-                            totalCost.addAndGet(getXPCost(enchant));
+                            totalCost.addAndGet(getXPCost(enchant, level, isBook));
                         }
 
-                    // if is new enchantment
+                        // if is new enchantment
                     } else {
                         newEnchantments.put(enchant, level);
-                        totalCost.addAndGet(getXPCost(enchant));
+                        totalCost.addAndGet(getXPCost(enchant, level, isBook));
                     }
                 }
             });
@@ -107,23 +120,131 @@ public class AnvilEnchantHandler {
             event.setOutput(out);
             event.setCost(totalCost.get());
             event.setMaterialCost(1);
-
-            // TODO: repairing items wont work. reimpliemnt that logic here
         }
     }
 
-    private static int getXPCost(Enchantment enchant){
-        switch(enchant.getRarity()) {
-            case COMMON:
-                return 1;
-            case UNCOMMON:
-                return 2;
-            case RARE:
-                return 4;
-            case VERY_RARE:
-                return 8;
+    public static void doRepairRename(AnvilUpdateEvent event){
+        ItemStack itemstack = event.getOutput();
+        int i = event.getCost();
+        int j = 0;
+        int k = 0;
+        ItemStack itemstack1 = itemstack.copy();
+        ItemStack itemstack2 = event.getRight();
+        j = j + itemstack.getRepairCost() + (itemstack2.isEmpty() ? 0 : itemstack2.getRepairCost());
+        int materialCost = 0;
+        int levelCost = 0;
+        boolean flag = false;
+
+        if (!itemstack2.isEmpty()) {
+            flag = itemstack2.getItem() == Items.ENCHANTED_BOOK && !EnchantedBookItem.getEnchantments(itemstack2).isEmpty();
+            if (itemstack1.isDamageable() && itemstack1.getItem().getIsRepairable(itemstack, itemstack2)) {
+                int l2 = Math.min(itemstack1.getDamage(), itemstack1.getMaxDamage() / 4);
+                if (l2 <= 0) {
+                    // this.field_234642_c_.setInventorySlotContents(0, ItemStack.EMPTY);
+                    levelCost = 0;
+                    return;
+                }
+
+                int i3;
+                for(i3 = 0; l2 > 0 && i3 < itemstack2.getCount(); ++i3) {
+                    int j3 = itemstack1.getDamage() - l2;
+                    itemstack1.setDamage(j3);
+                    ++i;
+                    l2 = Math.min(itemstack1.getDamage(), itemstack1.getMaxDamage() / 4);
+                }
+
+                materialCost = i3;
+            } else {
+                if (!flag && (itemstack1.getItem() != itemstack2.getItem() || !itemstack1.isDamageable())) {
+                    // this.field_234642_c_.setInventorySlotContents(0, ItemStack.EMPTY);
+                    levelCost = 0;
+                    return;
+                }
+
+                if (itemstack1.isDamageable() && !flag) {
+                    int l = itemstack.getMaxDamage() - itemstack.getDamage();
+                    int i1 = itemstack2.getMaxDamage() - itemstack2.getDamage();
+                    int j1 = i1 + itemstack1.getMaxDamage() * 12 / 100;
+                    int k1 = l + j1;
+                    int l1 = itemstack1.getMaxDamage() - k1;
+                    if (l1 < 0) {
+                        l1 = 0;
+                    }
+
+                    if (l1 < itemstack1.getDamage()) {
+                        itemstack1.setDamage(l1);
+                        i += 2;
+                    }
+                }
+            }
         }
 
-        return 0;
+        if (StringUtils.isBlank(event.getName())) {
+            if (itemstack.hasDisplayName()) {
+                k = 1;
+                i += k;
+                itemstack1.clearCustomName();
+            }
+        } else if (!event.getName().equals(itemstack.getDisplayName().getString())) {
+            k = 1;
+            i += k;
+            itemstack1.setDisplayName(new StringTextComponent(event.getName()));
+        }
+        if (flag && !itemstack1.isBookEnchantable(itemstack2)) itemstack1 = ItemStack.EMPTY;
+
+        levelCost = j + i;
+        if (i <= 0) {
+            itemstack1 = ItemStack.EMPTY;
+        }
+
+        if (k == i && k > 0 && levelCost >= 40) {
+            levelCost = 39;
+        }
+
+        if (levelCost >= 40 && !event.getPlayer().abilities.isCreativeMode) {
+            itemstack1 = ItemStack.EMPTY;
+        }
+
+        if (!itemstack1.isEmpty()) {
+            int k2 = itemstack1.getRepairCost();
+            if (!itemstack2.isEmpty() && k2 < itemstack2.getRepairCost()) {
+                k2 = itemstack2.getRepairCost();
+            }
+
+            if (k != i || k == 0) {
+                k2 = RepairContainer.getNewRepairCost(k2);
+            }
+
+            itemstack1.setRepairCost(k2);
+        }
+
+
+        event.setOutput(itemstack1);
+        event.setCost(levelCost);
+        if (materialCost > 0) event.setMaterialCost(materialCost);
+    }
+
+
+    private static int getXPCost(Enchantment enchant, int level, boolean isBook){
+        int cost = 0;
+        switch(enchant.getRarity()) {
+            case COMMON:
+                cost =  1;
+                break;
+            case UNCOMMON:
+                cost =  2;
+                break;
+            case RARE:
+                cost =  4;
+                break;
+            case VERY_RARE:
+                cost = 8;
+        }
+
+        if (isBook) {
+            cost = Math.max(1, cost / 2);
+        }
+
+        return cost * level;
     }
 }
